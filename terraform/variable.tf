e84b1f3adf945f3a1889acc79aedd1c2b2bd9c27
akash <akashsakore@gmail.com>
2026-07-22 03:37:51 +0000

    terraform configuration added

---
+++ terraform/variable.tf
@@ -0,0 +1,14 @@
+variable "aws_rigion" {
+  description = "aws rigion"
+  type = string
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "instance name"
+  type = string
+}
